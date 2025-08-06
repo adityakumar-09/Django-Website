c9fc2e58154218ce40438a958f84c6a0c0f3b560
adityakumar-09 <ashishadi2005@gmail.com>
2025-08-06 23:03:05 +0530

    tracker added

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
 from .models import Product, Contact, Orders
+from .models import OrderUpdate
 from math import ceil
-
-# Create your views here.
 from django.http import HttpResponse
+import json
 
 def index(request):
-    # products = Product.objects.all()
-    # print(products)
-    # n = len(products)
-    # nSlides = n//4 + ceil((n/4)-(n//4))
-
     allProds = []
     catprods = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catprods}
@@ -37,7 +32,25 @@ def contact(request):
     return render(request, "shop/contact.html")
 
 def tracker(request):
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps(updates, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+
     return render(request, 'shop/tracker.html')
+
 
 def search(request):
     return render(request, 'shop/search.html')
@@ -60,6 +73,8 @@ def checkout(request):
         order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
                        state=state, zip_code=zip_code, phone=phone)
         order.save()
+        update= OrderUpdate(order_id= order.order_id, update_desc="The order has been placed")
+        update.save()
         thank = True
         id = order.order_id
         return render(request, 'shop/checkout.html', {'thank':thank, 'id': id})
