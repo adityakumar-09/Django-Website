dfb7417bc337ed292be706825f8b3ad9ec6f3b94
adityakumar-09 <ashishadi2005@gmail.com>
2025-08-08 08:46:01 +0530

    blogpost page

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Blogpost(models.Model):
     head2 = models.CharField(max_length=500, default="")
     chead2 = models.CharField(max_length=5000, default="")
     pub_date = models.DateField()
+    writer = models.CharField(max_length=50, default="")
     thumbnail = models.ImageField(upload_to='shop/images', default="")
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from  django.http import HttpResponse
+from django.http import HttpResponse
+from .models import Blogpost
 def index(request):
-    return render(request,"blog/index.html")
+    myposts = Blogpost.objects.all()
+    return render(request,"blog/index.html",{'myposts': myposts})
 
-def blogpost(request):
-    return render(request,"blog/blogpost.html")
+def blogpost(request,id):
+    post = Blogpost.objects.filter(post_id=id).first()
+    return render(request,"blog/blogpost.html", {'post': post})
